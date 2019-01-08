bddf98c1b1c38560aa1e203ff1786ab556ac6783
Dakota Michael <dmichael080993@gmail.com>
2019-01-07 20:00:35 -0500

    Update view for bio info
    Eliminate rails logo
    
    Continued view changes
    
    More

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		flash[:success] = "Welcome to the Sample App!"
+  		flash[:success] = "Thank you for signing up, and welcome to the site!"
   		redirect_to @user
   	else
   		render 'new'
