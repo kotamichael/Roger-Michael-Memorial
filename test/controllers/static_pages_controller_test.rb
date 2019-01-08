fbd0785454c0bc455b17e1057f2df2a348e1e4ad
Dakota Michael <dmichael080993@gmail.com>
2019-01-08 00:20:50 -0500

    Login page part 1
    Update font colors for primary

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 	def setup
-		@base_title = "Ruby on Rails Tutorial Sample App"
+		@base_title = "Roger Mark Michael | In Memoriam"
 	end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Roger Mark Michael | In Memoriam"
   end
 
   test "should get help" do
